37a8331f62d3fa517689f6a7483dc39676ac75a2
sonish <sonishmzn95@gmail.com>
2018-12-07 16:25:50 +0545

    Blog Project completed

--- firstSite/blog/models.py
+++ firstSite/blog/models.py
@@ -4,17 +4,17 @@ from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     text  = models.TextField()
-    created_date= models.DateTimeField(default=timezone.now())
+    created_date= models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True,null= True)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-    def approve_comments(self):
-        return self.comments.filter(approved_comments= True)
+    def approved_comments(self):
+        return self.comments.filter(approved_comment= True)
 
     def __str__(self):
         return self.title
@@ -23,10 +23,10 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post',related_name="comments")
+    post = models.ForeignKey('blog.Post',related_name="comments",on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default= timezone.now())
+    created_date = models.DateTimeField(default= timezone.now)
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
--- firstSite/firstSite/urls.py
+++ firstSite/firstSite/urls.py
@@ -17,7 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 
+from django.contrib.auth import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'',include("blog.urls")),
+    url(r'accounts/login/$',views.LoginView.as_view(),name="login"),
+    url(r'accounts/logout/$',views.LogoutView.as_view(),name="logout",kwargs={'next_page':'/'}),
+
 ]
